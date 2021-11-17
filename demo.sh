a4a904b7a53d85d0104c17985cf5dc11367beb08
Wojciech Trocki <wtrocki@redhat.com>
2021-11-17 13:35:33 +0000

    fix: remove demo script

--- demo.sh
+++
@@ -1,8 +0,0 @@
-
-rhoas profile generate --file-format env  
-rhoas profile generate --file-format helm 
-rhoas profile generate --file-format json 
-rhoas profile generate --file-format kube
-rhoas profile generate --file-format rhoas
-
-## 
