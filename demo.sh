82b7ba5e31e0a0af9e2091bffe89ac6acc571d45
Wojciech Trocki <wtrocki@redhat.com>
2021-11-17 13:23:32 +0000

    fix: minor adjustment

---
+++ demo.sh
@@ -0,0 +1,8 @@
+
+rhoas profile generate --file-format env  
+rhoas profile generate --file-format helm 
+rhoas profile generate --file-format json 
+rhoas profile generate --file-format kube
+rhoas profile generate --file-format rhoas
+
+## 
